470d9dcbe88dab4c9ed93716b96df46872afeee8
AlejandroZaSa <az0031456@gmail.com>
2024-04-03 09:33:01 -0500

    despliegue

---
+++ src/main/java/co/edu/uniquindio/barberiavip/modelo/entidades/SolicitudCita.java
@@ -0,0 +1,43 @@
+package co.edu.uniquindio.barberiavip.modelo.entidades;
+
+import co.edu.uniquindio.barberiavip.modelo.enums.Estado;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class SolicitudCita {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private float costo;
+
+    @Column(nullable = false)
+    private LocalDate fecha;
+
+    @Column(nullable = false)
+    private Estado estado;
+
+    @JoinColumn(nullable = false)
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToMany
+    private Set<Servicio> servicios;
+
+    @OneToOne(mappedBy = "solicitudCita")
+    private Pago pago;
+
+}
